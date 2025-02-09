eaad1f172c0646f0f08026e6a73585026f8a19ba
kennyrukoro <k.rukoro@alustudent.com>
2025-02-09 12:23:18 -0800

    optional script to create both files in analyzed_data

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 #Command to Obtain highest temperatures
 tail -n +2 ./raw_data/satelite_temperature_data.csv | sort -t, -k3,3nr | head -n 10 > analyzed_data/highest_temp.csv
+
 #Comand to capture the data of a single country which is Rwanda
 awk -F, '$1 == "Rwanda"' raw_data/satelite_temperature_data.csv | sort -t, -k4,4nr > analyzed_data/humidity_data_Rwanda.csv
